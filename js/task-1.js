62cf40bf5a34b535005078cab1357de4900abb8d
MAKCIMELIANO <makcimeliano@gmail.com>
2024-09-19 14:34:22 +0300

    goit-js-hw-3

---
+++ js/task-1.js
@@ -0,0 +1,9 @@
+console.log('\x1b[33m=====TASK-1=====\x1b[0m');
+function slugify(string) {
+  return string.toLowerCase().split(' ').join('-');
+}
+
+console.log(slugify('Arrays for beginners')); // "arrays-for-beginners"
+console.log(slugify('English for developer')); // "english-for-developer"
+console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript"
+console.log(slugify('How to become a JUNIOR developer in TWO WEEKS')); // "how-to-become-a-junior-developer-in-two-weeks"
---
+++ js/task-3.js
@@ -0,0 +1,16 @@
+console.log('\x1b[33m=====TASK-3=====\x1b[0m');
+function filterArray(numbers, value) {
+  const newArray = [];
+  for (let i = 0; i < numbers.length; i += 1) {
+    if (numbers[i] > value) {
+      newArray.push(numbers[i]);
+    }
+  }
+  return newArray;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
